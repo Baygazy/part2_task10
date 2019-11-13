039f686bb676f247acce6fbda103e32359cf4120
Baygazy <bgzmkbull@gmail.com>
2019-11-13 11:34:51 +0600

    part2_task10

---
+++ task10.py
@@ -0,0 +1,11 @@
+# spisok1 = input()
+spisok = "123456"
+print(spisok[2])
+print(spisok[-2])
+print(spisok[:5])
+print(spisok[:-2])
+print(spisok[1::2])
+print(spisok[::2])
+print(spisok[::-1])
+print(spisok[-2::-2])
+print(len(spisok))
